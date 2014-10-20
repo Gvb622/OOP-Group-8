ab35692596c2bc95421ac239364fe72a454d6b42
Axel <axel.rl.404@gmail.com>
2014-10-20 18:48:27 +0700

    Create Burning.java class
    class for checking if the grid position has searched or not
    false if not
    true if yes

---
+++ Meeting/Burning.java
@@ -0,0 +1,18 @@
+package fireproject;
+
+public class Burning{
+    boolean burn[][];
+    
+    public Burning(int x,int y){
+        burn = new boolean [x+2][y+2];
+        reset();
+    }
+    
+    public  void reset(){
+        for(int i=0;i<burn.length;i++){
+            for(int j=0;j<burn[0].length;j++){
+                burn[i][j]=false;
+            }
+        }
+    }
+}
